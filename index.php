e063416ba10bc0ef9538457a0ad9061cba60a77b
nofikoffgit <ruslan.novikov@gmail.com>
2021-01-06 02:08:58 +0200

    акутализировал все ссылки на файлы и их даты последнего изменения

--- index.php
+++ index.php
@@ -1,15 +1,150 @@
 <?php
 $list_regions = [
     [
-        'fileURL' => 'https://kyivobl.tax.gov.ua/data/files/253906.RAR',
-        'pageURL' => 'https://kyivobl.tax.gov.ua/byudjetni-rahunki/',
-        'pageRegexp' => '~<strong>1\. <a href="([^"]+)">~',
+        'pageURL' => 'https://kyivobl.tax.gov.ua/okremi-storinki/arhiv2/447490.html',
+        'fileURL' => 'https://kyivobl.tax.gov.ua/data/material/000/352/447490/Rahunki_DB_Kievska.xls',
+        'fileDate' => '05-01-2021 08:36:22',
+        'fileURL2' => 'https://kyivobl.tax.gov.ua/data/material/000/352/447490/Rahunki_MB_Kievska.xlsx',
+        'fileDate2' => '05-01-2021 08:36:22',
     ],
 
     [
-        'fileURL' => 'https://kyivobl.tax.gov.ua/data/files/253906.RAR',
-        'pageURL' => 'https://kyivobl.tax.gov.ua/byudjetni-rahunki/',
-        'pageRegexp' => '~<strong>1\. <a href="([^"]+)">~',
+        'pageURL' => 'https://kyiv.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://kyiv.tax.gov.ua/data/files/253961.xls',
+        'fileDate' => '30-12-2020 12:44:21',
+    ],
+
+    [
+        'pageURL' => 'http://vin.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://vin.tax.gov.ua/data/material/000/351/446417/DB_2021.xlsx',
+        'fileDate' => '24-12-2020 15:32:58',
+        'fileURL2' => 'https://vin.tax.gov.ua/data/material/000/351/446417/mb_2021.xlsx',
+        'fileDate2' => '24-12-2020 15:32:58',
+    ],
+
+    [
+        'pageURL' => 'http://ch.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://ch.tax.gov.ua/data/files/253901.xls',
+        'fileDate' => '24-12-2020 13:28:57',
+    ],
+    [
+        'pageURL' => 'http://cv.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://cv.tax.gov.ua/data/material/000/351/446286/Dodatok_1_derzh_24.xls',
+        'fileDate' => '29-12-2020 13:16:32',
+        'fileURL2' => 'https://cv.tax.gov.ua/data/material/000/351/446289/Dodatok_2_mists_24.xls',
+        'fileDate2' => '24-12-2020 10:07:52',
+    ],
+    [
+        'pageURL' => 'http://ck.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://ck.tax.gov.ua/data/material/000/010/67220/01012021.xls',
+        'fileDate' => '24-12-2020 14:40:36',
+    ],
+    [
+        'pageURL' => 'http://km.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://km.tax.gov.ua/data/files/254035.xlsx',
+        'fileDate' => '05-01-2021 16:56:42',
+        'fileURL2' => 'https://km.tax.gov.ua/data/files/254036.xlsx',
+        'fileDate2' => '05-01-2021 16:56:57',
+    ],
+    [
+        'pageURL' => 'http://kherson.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://kherson.tax.gov.ua/data/files/254014.xls',
+        'fileDate' => '04-01-2021 09:56:45',
+    ],
+    [
+        'pageURL' => 'http://kh.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://kh.tax.gov.ua/data/files/254020.xls',
+        'fileDate' => '04-01-2021 13:24:22',
+    ],
+    [
+        'pageURL' => 'http://tr.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://tr.tax.gov.ua/data/material/000/351/446350/Ternopil_DB.xls',
+        'fileDate' => '05-01-2021 16:15:56',
+        'fileURL2' => 'https://tr.tax.gov.ua/data/material/000/351/446350/Ternopil_MB.xls',
+        'fileDate2' => '05-01-2021 16:17:36',
+    ],
+    [
+        'pageURL' => 'http://sumy.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://sumy.tax.gov.ua/data/files/253894.xls',
+        'fileDate' => '23-12-2020 16:27:13',
+    ],
+    [
+        'pageURL' => 'http://rv.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://rv.tax.gov.ua/data/files/253899.xls',
+        'fileDate' => '24-12-2020 11:20:25',
+    ],
+    [
+        'pageURL' => 'http://poltava.tax.gov.ua/byudjetni-rahunki/',
+        // это не ошибка - это дятлы в полтаве домен свой не знают
+        //'fileURL2' => 'https://kh.tax.gov.ua/data/files/254040.xls',
+        'fileURL' => 'https://poltava.tax.gov.ua/data/files/254040.xls',
+        'fileDate' => '05-01-2021 18:17:04',
+    ],
+    [
+        'pageURL' => 'http://od.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'http://od.tax.gov.ua/data/files/253928.xls',
+        'fileDate' => '05-01-2021 16:50:31',
+    ],
+    [
+        'pageURL' => 'http://mk.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://mk.tax.gov.ua/data/files/254024.xls',
+        'fileDate' => '04-01-2021 14:56:13',
+        'fileURL2' => 'https://mk.tax.gov.ua/data/files/254029.xls',
+        'fileDate2' => '05-01-2021 10:58:19',
+    ],
+    [
+        'pageURL' => 'http://lv.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://lv.tax.gov.ua/data/files/253919.xlsx',
+        'fileDate' => '28-12-2020 10:07:54',
+    ],
+    [
+        'pageURL' => 'http://lg.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://lg.tax.gov.ua/data/files/253927.xls',
+        'fileDate' => '29-12-2020 07:37:55',
+    ],
+    [
+        'pageURL' => 'http://kr.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://kr.tax.gov.ua/data/material/000/281/365160/Svod_rahunk_v_2021.xls',
+        'fileDate' => '28-12-2020 10:33:54',
+    ],
+    [
+        'pageURL' => 'http://if.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://if.tax.gov.ua/data/material/000/351/446397/Nov_rahunki_z_1.01.2021_roku.XLS',
+        'fileDate' => '24-12-2020 14:54:26',
+    ],
+    [
+        'pageURL' => 'http://zp.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://zp.tax.gov.ua/data/normativ/000/003/74954/Rahunki_2021_DB_MB_20210104155724781.xls',
+        'fileDate' => '04-01-2021 15:57:24',
+    ],
+    [
+        'pageURL' => 'http://zak.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://zak.tax.gov.ua/data/files/253981.xls',
+        'fileDate' => '31-12-2020 09:58:46',
+    ],
+    [
+        'pageURL' => 'http://zt.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://zt.tax.gov.ua/data/material/000/351/446386/DB2021_zagalna.xls',
+        'fileDate' => '24-12-2020 14:34:51',
+        'fileURL2' => 'https://zt.tax.gov.ua/data/material/000/351/446386/MB2021_zagalna.xls',
+        'fileDate2' => '24-12-2020 14:34:59',
+    ],
+    [
+        'pageURL' => 'http://dn.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://dn.tax.gov.ua/data/material/000/331/414335/rahunki_05.xls',
+        'fileDate' => '05-01-2021 13:01:47',
+    ],
+    [
+        'pageURL' => 'http://dp.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'https://dp.tax.gov.ua/data/files/253910.xls',
+        'fileDate' => '28-12-2020 08:43:01',
+    ],
+    [
+        'pageURL' => 'http://vl.tax.gov.ua/byudjetni-rahunki/',
+        'fileURL' => 'http://vl.tax.gov.ua/data/files/253890.xls',
+        'fileDate' => '23-12-2020 16:24:06',
+        'fileURL2' => 'http://vl.tax.gov.ua/data/files/253891.xls',
+        'fileDate2' => '23-12-2020 16:24:32',
     ],
 
 ];
@@ -20,30 +155,39 @@ foreach ($list_regions as $list_region) {
 
 function print_check($region)
 {
-    echo "Анализ {$region['pageURL']} на предмет смены ссылки на файл<br>\n";
-
-
-    $content = file_get_contents($region['pageURL']);
-    if (preg_match($region['pageRegexp'], $content, $d)) {
-
-        // ищем старую ссылку вида /data/files/253906.RAR
-        if ($d[1] !== str_replace('https://kyivobl.tax.gov.ua', '', $region['fileURL'])) {
-            echo "Ура. Ссылка изменилась новая " . $d[1];
-        } else {
-            echo "К сожалению нового файла нет, старый " . $d[1];
-        }
-
-    } else {
-        echo "Не могу прочитать страницу";
+    echo "Анализ <a href='{$region['pageURL']}'>{$region['pageURL']}</a> на предмет смены ссылки на файл<br>\n";
+    if (!$region['fileURL']) {
+        echo "не указан адрес файла<br>\n";
+        return;
     }
 
-// Определяем и выводим Last-Modified создания файла
-    $headers = get_headers($region['fileURL'], 1);
+    $content = file_get_contents($region['pageURL']);
+
+    checkOneFile($content, $region['fileURL'], $region['fileDate']);
+    // у неокторых два файла надо чекать
+    if (!empty($region['fileURL2'])) {
+        checkOneFile($content, $region['fileURL2'], $region['fileDate2']);
+    }
+
+
+}
+
+function checkOneFile($content, $file, $fileDate)
+{
+    if (!strpos($content, parse_url($file)['path'])) {
+        echo "<font color=red>  !! эта ссылка теперь отсуствует на странице налоговой' {$file} : </font>\n";
+    } else {
+        echo "сылка на файл не изменилась : \n";
+    }
+    // Определяем и выводим Last-Modified создания файла
+    $headers = get_headers($file, 1);
     $date = "Error";
-//echo "<pre>"; print_r($headers); echo "</pre>";
     if ($headers && (strpos($headers[0], '200') !== FALSE)) {
         $time = strtotime($headers['Last-Modified']);
         $date = date("d-m-Y H:i:s", $time);
     }
-    echo '<br> ' . $region['fileURL'] . ' last-Modified : ' . $date . '<br>';
+    echo '<br> ' . $file . ' last-Modified : <strong>' . $date . '</strong><br>';
+    if ($fileDate && $date !== 'Error' && $fileDate !== $date)
+        echo "<font color=red>  !! Дата файла изменилась {$file} (старая была {$fileDate})<br></font>\n";
+
 }
